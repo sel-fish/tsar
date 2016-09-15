6b1a7ca58a089a402e3066c435649fa86c82f43e
sel-fish <fqlgy@hotmail.com>
2016-09-15 20:09:22 +0800

    add support for multi redis insts

--- modules/mod_redis.c
+++ modules/mod_redis.c
@@ -21,34 +21,73 @@
 #include <arpa/inet.h>
 #include "tsar.h"
 
-#define STATS_TEST_SIZE (sizeof(struct stats_redis))
+
+#define MAX_INSTANCES 30
+#define LEN_4096 4096
 
 static const char *redis_usage = "    --redis               redis information";
+
+struct redis_instance_info {
+    int port;
+} redis_instances[MAX_INSTANCES];
 
 /*
  * temp structure for collection information.
  */
 struct stats_redis {
-    unsigned long long keys;             /* keys in db */
-};
+    unsigned long long keys;
+    /* keys in db */
+    unsigned long long expires;
+    /* expire keys in db */
+    unsigned long long ops;              /* operations per second */
+} redis_stats[MAX_INSTANCES];
+
+unsigned int n_instances = 0;                /* Number of instances */
 
 /* Structure for tsar */
 static struct mod_info redis_info[] = {
-        {"keys", SUMMARY_BIT, 0, STATS_NULL},
+        {"   keys", SUMMARY_BIT, MERGE_SUM, STATS_NULL},
+        {"expires", SUMMARY_BIT, MERGE_SUM, STATS_NULL},
+        {"    ops", SUMMARY_BIT, MERGE_SUM, STATS_NULL}
 };
 
-static void
-read_redis_stats(struct module *mod, const char *parameter) {
-    int write_flag = 0, addr_len, domain;
-    int m, sockfd, send, pos;
+int redis_inst_initialize() {
+    char *cmd = "ss -4ntlp 2>/dev/null |awk '{split($4,port,\":\"); "
+            "if($NF~/redis-server/) print port[length(port)]}' 2>/dev/null";
+    char buf[LEN_4096];
+    FILE *fp;
+
+    if ((fp = popen(cmd, "r")) == NULL) {
+        printf("Error opening pipe!\n");
+        return -1;
+    }
+
+    int i = 0;
+    while (fgets(buf, LEN_4096, fp) != NULL) {
+        redis_instances[i].port = atoi(buf);
+        if (++i >= MAX_INSTANCES) {
+            break;
+        }
+    }
+    n_instances = i;
+
+    if (pclose(fp)) {
+        printf("Command not found or exited with error status\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+void collect_redis_inst_stat(int index) {
+    int addr_len, domain, sockfd;
     void *addr;
     char buf[LEN_4096], request[LEN_4096], line[LEN_4096];
     FILE *stream = NULL;
-
     struct sockaddr_in servaddr;
 
-    char *host = "127.0.0.1";
-    int port = 6379;
+    char *host = "localhost";
+    int port = redis_instances[index].port;
 
     /* parameter actually equals to mod->parameter */
     struct stats_redis st_redis;
@@ -68,13 +107,13 @@ read_redis_stats(struct module *mod, const char *parameter) {
         goto writebuf;
     }
 
-    if ((m = connect(sockfd, (struct sockaddr *) addr, addr_len)) == -1) {
+    if ((connect(sockfd, (struct sockaddr *) addr, addr_len)) == -1) {
         goto writebuf;
     }
 
     sprintf(request, "%s", "*2\r\n$4\r\ninfo\r\n$3\r\nall\r\n");
 
-    if ((send = write(sockfd, request, strlen(request))) == -1) {
+    if ((write(sockfd, request, strlen(request))) == -1) {
         goto writebuf;
     }
 
@@ -102,7 +141,11 @@ read_redis_stats(struct module *mod, const char *parameter) {
         goto writebuf;
     }
 
-    unsigned long long keys = 0, cur_keys = 0, db, expires, avg_ttl;
+    unsigned long long keys = 0, cur_keys,
+            expires = 0, cur_expires,
+            ops = 0,
+            db, avg_ttl;
+
     int keyspace_field = 0;
 
     // read all content of bulk replay
@@ -111,14 +154,16 @@ read_redis_stats(struct module *mod, const char *parameter) {
     while (fgets(line, LEN_4096, stream) != NULL) {
         if (!strncmp(line, "# Keyspace", sizeof("# Keyspace") - 1)) {
             keyspace_field = 1;
-        } else if (1 == keyspace_field) {
-            if (!strncmp(line, "db", sizeof("db") - 1)) {
+        } else if (!strncmp(line, "db", sizeof("db") - 1)) {
+            if (1 == keyspace_field) {
                 // format --> db0:keys=3,expires=0,avg_ttl=0
                 sscanf(line, "db%llu:keys=%llu,expires=%llu,avg_ttl=%llu",
-                       &db, &cur_keys, &expires, &avg_ttl);
+                       &db, &cur_keys, &cur_expires, &avg_ttl);
                 keys += cur_keys;
-                write_flag = 1;
+                expires += cur_expires;
             }
+        } else if (!strncmp(line, "instantaneous_ops_per_sec", sizeof("instantaneous_ops_per_sec") - 1)) {
+            sscanf(line, "instantaneous_ops_per_sec:%llu", &ops);
         }
 
         // update read len
@@ -128,7 +173,9 @@ read_redis_stats(struct module *mod, const char *parameter) {
         }
     }
 
-    st_redis.keys = keys;
+    redis_stats[index].keys = keys;
+    redis_stats[index].expires = expires;
+    redis_stats[index].ops = ops;
 
     writebuf:
     if (stream) {
@@ -138,16 +185,45 @@ read_redis_stats(struct module *mod, const char *parameter) {
     if (sockfd != -1) {
         close(sockfd);
     }
+}
 
-    if (1 == write_flag) {
-        pos = sprintf(buf, "%llu",
-        /* the store order is not same as read procedure */
-                      st_redis.keys);
+static void print_instance_stats(struct module *mod) {
+    int pos = 0;
+    char buf[LEN_1M];
+    memset(buf, 0, LEN_1M);
+    unsigned int i;
 
-        buf[pos] = '\0';
-        /* send data to tsar you can get it by pre_array&cur_array at set_redis_record */
-        set_mod_record(mod, buf);
+    for (i = 0; i < n_instances; i++) {
+        pos += snprintf(buf + pos, LEN_1M - pos, "%d=%llu,%llu,%llu,%d" ITEM_SPLIT,
+                        redis_instances[i].port,
+                        redis_stats[i].keys,
+                        redis_stats[i].expires,
+                        redis_stats[i].ops,
+                        pos);
+        if (strlen(buf) == LEN_1M - 1) {
+            return;
+        }
     }
+
+    buf[pos] = '\0';
+    set_mod_record(mod, buf);
+    return;
+}
+
+static void
+read_redis_stats(struct module *mod, const char *parameter) {
+    // get instance num and port
+    if (0 != redis_inst_initialize()) {
+        return;
+    }
+
+    // collect stat of all redis instances
+    unsigned int i = 0;
+    for (i = 0; i < n_instances; i++) {
+        collect_redis_inst_stat(i);
+    }
+
+    print_instance_stats(mod);
 
     return;
 }
@@ -165,5 +241,5 @@ set_redis_record(struct module *mod, double st_array[],
 /* register mod to tsar */
 void
 mod_register(struct module *mod) {
-    register_mod_fields(mod, "--redis", redis_usage, redis_info, 3, read_redis_stats, set_redis_record);
+    register_mod_fields(mod, "--redis", redis_usage, redis_info, 4, read_redis_stats, set_redis_record);
 }
